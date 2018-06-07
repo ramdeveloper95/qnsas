d2b3c217d7755646ed93fb1ab4338d94d931300c
Programador Ricardo Alvarez <ramdeveloper95@outlook.es>
2018-06-07 02:24:08 -0500

    Cambios
    Home
    Nosotros
    Servicios
    Clientes

--- js/functions.js
+++ js/functions.js
@@ -1,57 +1,63 @@
 
 $(document).ready(function() {
-	$("#description-abouts").show();
+	$("#description-abouts").slideDown();
     $(".abouts-div").hide();
 
 });
 
 function abouts(id){
 
-	$(".enabled").css("display", "none");
+  /*Titulo del item*/
+  title = $("#" + id).html();
 
-	$("#description-" + id).fadeIn();
+  /*Oculta y muestra items sub menu*/
+	$(".enabled").slideUp("slow");
+
+	$("#description-" + id).slideDown("slow");
 
 	$("#description-" + id).removeClass('disabled');
 	$("#description-" + id).addClass('enabled');
+
+  $("#title-abouts").html(title);
 }
 
 $(document).ready(function() {
 
   $("#next").on("click", function() {
     if($('#service1').is(':visible')) {
-      $("#service1").css("display", "none");
-      $("#service2").fadeIn();
+      $("#service1").slideUp("slow");
+      $("#service2").slideDown("slow");
       $(".fa-chevron-left").css({
         opacity: 1,
         cursor: "pointer"
       });
     }
     else if($('#service2').is(':visible')) {
-      $("#service2").css("display", "none");
+      $("#service2").slideUp("slow");
       $(".fa-chevron-right").css({
         opacity: 0.3,
         cursor: "default"
       });
-      $("#service3").fadeIn();
+      $("#service3").slideDown("slow");
     }
   });
 
   $("#previous").on("click", function() {
     if($('#service3').is(':visible')) {
-      $("#service3").css("display", "none");
-      $("#service2").fadeIn();
+      $("#service3").slideUp("slow");
+      $("#service2").slideDown("slow");
       $(".fa-chevron-right").css({
         opacity: 1,
         cursor: "pointer"
       });
     }
     else if($('#service2').is(':visible')) {
-      $("#service2").css("display", "none");
+      $("#service2").slideUp("slow");
       $(".fa-chevron-left").css({
         opacity: 0.3,
         cursor: "default"
       });
-      $("#service1").fadeIn();
+      $("#service1").slideDown("slow");
     }
   });
 
